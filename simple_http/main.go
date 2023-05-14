ef71b3fa4229ea32d5e03b88796ff685f1d3e6b0
Zane <44780287+betterlmy@users.noreply.github.com>
2023-05-14 16:51:58 +0800

    1

--- simple_http/main.go
+++ simple_http/main.go
@@ -1,12 +1,29 @@
+// The code creates a channel, ch, and two goroutines.
+// The first goroutine creates a channel, ch, and sends a 1 to it.
+// The second goroutine receives from the channel, ch, after a second.
+// The main goroutine prints the number of goroutines in the program every second.
+
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
-	// /代表着传进来任何的URI都执行返回helloword,因为会将pattern进行最长前缀匹配,匹配成功后,执行对应的处理函数.
-	http.ListenAndServe(":8080", nil)
-
+	var ch chan int
+	go func() {
+		ch = make(chan int, 1)
+		ch <- 1
+	}()
+	go func(ch chan int) {
+		time.Sleep(time.Second)
+		<-ch
+	}(ch)
+	c := time.Tick(1 * time.Second)
+	for range c {
+		// print the number of goroutines in the program
+		fmt.Printf("#goroutines: %d\n", runtime.NumGoroutine())
+	}
 }
